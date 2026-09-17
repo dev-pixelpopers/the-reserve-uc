d634774e0134b6f07695df521a0dc120d2e4213a
Tommy Vercetti <88341238+MIAN-SAHAB@users.noreply.github.com>
2026-09-17 18:29:06 +0500

    Updates

---
+++ src/app/what-we-offer/page.tsx
@@ -0,0 +1,9 @@
+import WhatWeOffer from "@/components/WhatWeOffer";
+
+export default function WhatWeOfferPage() {
+    return (
+        <main className="bg-[#000000] hero-sec">
+            <WhatWeOffer />
+        </main>
+    );
+}
